7ba127158b0bb1eb04b5725066f0be9f6dc3ad8e
chamma75 <gw4e@users.noreply.github.com>
2017-06-08 18:35:58 +0200

    prepare auto deployment

--- upload.sh
+++ upload.sh
@@ -35,12 +35,6 @@ rm -rf out/**/* || exit 0
 
 unzip $HOME/.m2/repository/org/gw4e/tycho/org.gw4e.tycho.update/4.0.0-SNAPSHOT/org.gw4e.tycho.update-4.0.0-SNAPSHOT.zip -d ./out
 
-# If there are no changes to the compiled out (e.g. this is a README update) then just bail.
-if git diff --quiet; then
-    echo "No changes to the output on this push; exiting."
-    exit 0
-fi
-
 # Commit the "changes", i.e. the new version.
 # The delta will show diffs between new and old versions.
 git add -A .
