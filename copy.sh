3fb452c6cba79e8850b4c6218ee0b6af8facba69
chamma75 <gw4e@users.noreply.github.com>
2017-06-08 08:26:02 +0200

    update build for uploading folders

--- copy.sh
+++ copy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-
-cp $HOME/.m2/repository/org/gw4e/tycho/org.gw4e.tycho.update/4.0.0-SNAPSHOT/org.gw4e.tycho.update-4.0.0-SNAPSHOT.zip $TRAVIS_BUILD_DIR
-
+mkdir p2
+mkdir p2/p2repo
+unzip $HOME/.m2/repository/org/gw4e/tycho/org.gw4e.tycho.update/4.0.0-SNAPSHOT/org.gw4e.tycho.update-4.0.0-SNAPSHOT.zip -d p2/p2repo
 
