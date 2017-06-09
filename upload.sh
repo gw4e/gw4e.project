f6664b6bb1e3c897942094f9c256718bcd4e5559
chamma75 <hamma_chabane@hotmail.com>
2017-06-09 15:34:38 +0200

    update upload version

--- upload.sh
+++ upload.sh
@@ -4,7 +4,7 @@ set -e # Exit with nonzero exit code if anything fails
 
 
 SOURCE_BRANCH="master"
-TARGET_BRANCH="p2repo"
+TARGET_BRANCH="p2repo-4.0.0"
 
 
 # Pull requests and commits to other branches shouldn't try to deploy, just build to verify
@@ -19,10 +19,6 @@ REPO=`git config remote.origin.url`
 SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
 SHA=`git rev-parse --verify HEAD`
 
-echo XXXXXXXXX $REPO
-echo XXXXXXXXX $SSH_REPO
-echo XXXXXXXXX $SHA
-
 # Clone the existing branch for this repo into out/
 # Create a new empty branch if TARGET_BRANCH doesn't exist yet (should only happen on first deply)
 git clone $REPO out
@@ -35,28 +31,14 @@ rm -rf out/* || exit 0
 
 mkdir ./out/repository
 
-echo XXXXXXXXXPWD
-pwd
-echo XXXXXXXXXLS
-ls
-echo XXXXXXXXX
-
 unzip $HOME/.m2/repository/org/gw4e/tycho/org.gw4e.tycho.update/4.0.0-SNAPSHOT/org.gw4e.tycho.update-4.0.0-SNAPSHOT.zip -d ./out/repository
 
 cd out
-pwd 
+
 # Commit the "changes", i.e. the new version.
 # The delta will show diffs between new and old versions.
 git add -A .
-echo XXXXXX 3
-git status
-echo XXXXXX 4
 git commit -m "Deploy to GitHub : ${SHA}"
-echo XXXXXX 5
-
-git show-ref
-echo XXXXXX 6 $SSH_REPO $TARGET_BRANCH
-
 
 # Now that we're all set up, we can push.
 git push "https://${GH_TOKEN}@github.com/${GITHUB_REPO}.git" $TARGET_BRANCH
