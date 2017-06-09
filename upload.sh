ac9edbbd42d1395441ec559de60adea23c69a265
chamma75 <hamma_chabane@hotmail.com>
2017-06-09 11:15:56 +0200

    update upload

--- upload.sh
+++ upload.sh
@@ -35,6 +35,7 @@ rm -rf out/**/* || exit 0
 
 unzip $HOME/.m2/repository/org/gw4e/tycho/org.gw4e.tycho.update/4.0.0-SNAPSHOT/org.gw4e.tycho.update-4.0.0-SNAPSHOT.zip -d ./out
 
+cd out
 echo XXXXXX 1
 pwd 
 echo XXXXXX 2
