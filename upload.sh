4d65f343e1afe39ce03c75ac06405dc3b2eea7d1
chamma75 <hamma_chabane@hotmail.com>
2017-06-09 11:45:13 +0200

    update upload

--- upload.sh
+++ upload.sh
@@ -51,18 +51,9 @@ echo XXXXXX 5
 git show-ref
 echo XXXXXX 6 $SSH_REPO $TARGET_BRANCH
 
- # Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
-ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
-ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
-ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
-ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
-openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in id_rsa.pub.enc -out deploy_key -d
-chmod 600 deploy_key
-eval `ssh-agent -s`
-ssh-add deploy_key
 
 # Now that we're all set up, we can push.
-git push $SSH_REPO $TARGET_BRANCH
+git push "https://${GH_TOKEN}@$github.com/${GITHUB_REPO}.git" $TARGET_BRANCH
 
 
 
